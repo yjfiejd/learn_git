ab437d1b6668394bdbe0b5e355ddc4c98376bceb
liuxiaochen <yjfiejd@163.com>
2018-10-31 11:46:41 +0800

    new branch

--- Desktop/map_concept_python3/build/lib/tsunagi/__init__.py
+++
@@ -1,2 +0,0 @@
-import ldk_proxy
-from .tsunagi import get_disease_concept_id, get_concept_id, get_redundancy, get_ancestor, get_id_attr, get_unit_code, get_max_match, multi_concept, domain
--- Desktop/map_concept_python3/test.py
+++
@@ -1,7 +0,0 @@
-from lib.map_concept import MapConcept
-import os
-test =MapConcept()
-#print(os.path.abspath(__file__))
-a = os.path.join(os.path.dirname(__file__))
-#print(a) 
-print(test.get_disease_concept_id("糖尿病"))
